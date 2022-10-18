978de8a8fe7992b70a079e4766f327cd395f0860
Noah Taylor <epicplayvig@gmail.com>
2022-10-18 18:03:37 -0400

    Small Fixes

--- scripts/Tooltips.js
+++ scripts/Tooltips.js
@@ -8,7 +8,7 @@ function SetupTooltips() {
             let Size = Tooltip.getBoundingClientRect();
             let ParentSize = Curr.getBoundingClientRect()
             Tooltip.style.left = (ParentSize.width / 2 + ParentSize.x - Size.width / 2) + "px"
-            Tooltip.style.top = (ParentSize.height / 2 + ParentSize.y + Size.height - 7) + "px"
+            Tooltip.style.top = (ParentSize.height / 2 + ParentSize.y + Size.height - 6) + "px"
             document.getElementById("TooltipText").classList.remove("TooltipInactive")
             document.getElementById("TooltipText").classList.add("TooltipActive")
         })

589867410b695c3b4c1a8a4c714f5f781efea319
Strayfade <epicplayvig@gmail.com>
2022-11-01 08:41:59 -0400

    Accessibility Improvements

--- generators/Header.js
+++ generators/Header.js
@@ -9,6 +9,7 @@ function GenerateHeader(Article, Locale, isHomepage = false) {
     Output += "<a aria-label='" + Localize(Locale, "header_main") + "' class='LinkNormal' href='/' data-tooltip='" + Localize(Locale, "header_main") + "'>"
     Output += "<div class='HeaderObject HoverAccentColor'>"
     Output += "<svg class='Icon NoSelect NoSpacing' version='1.0' xmlns='http://www.w3.org/2000/svg' width='300.000000pt' height='300.000000pt' viewBox='0 0 300.000000 300.000000' preserveAspectRatio='xMidYMid meet'>"
+    Output += "<title>Strayfade Website Logo</title>"
     Output += "<g transform='translate(0.000000,300.000000) scale(0.050000,-0.050000)'>"
     Output += "<path d='M2707 5344 c-178 -217 -563 -686 -857 -1043 -609 -741 -676 -830 -786 -1052 -265 -529 -186 -1034 228 -1460 114 -117 678 -590 702 -588 9 1 372 434 806 964 1176 1433 1361 1655 1376 1655 73 0 193 -307 179 -459 -16 -176 -34 -201 -1014 -1389 -506 -615 -920 -1123 -920 -1130 2 -19 587 -490 606 -487 9 1 194 217 410 480 216 263 596 725 844 1027 679 823 798 1012 870 1376 103 514 -94 878 -779 1439 l-277 227 -46 -47 c-42 -43 -814 -980 -1749 -2122 -203 -249 -380 -453 -393 -454 -40 -2 -147 163 -180 276 -75 257 -63 276 863 1400 429 521 842 1022 917 1114 75 92 131 176 124 187 -17 28 -549 463 -577 472 -13 4 -169 -169 -347 -386z'/>"
     Output += "</g>"
@@ -28,12 +29,14 @@ function GenerateHeader(Article, Locale, isHomepage = false) {
     Output += "</div>"
     Output += "</a>"
 
-    Output += "<div id='LightModeToggleLight' class='HeaderObject HoverAccentColor CursorPointer Hidden' onclick='SetLightMode(!UseLightMode)' data-tooltip='" + Localize(Locale, "tooltip_darkmode") + "'>"
+    Output += "<div tabindex='0' onclick='SetLightMode(!UseLightMode)'>"
+    Output += "<div id='LightModeToggleLight' class='HeaderObject HoverAccentColor CursorPointer Hidden' data-tooltip='" + Localize(Locale, "tooltip_darkmode") + "'>"
     Output += `<svg class="HeaderSpecial" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M272 384c9.6-31.9 29.5-59.1 49.2-86.2l0 0c5.2-7.1 10.4-14.2 15.4-21.4c19.8-28.5 31.4-63 31.4-100.3C368 78.8 289.2 0 192 0S16 78.8 16 176c0 37.3 11.6 71.9 31.4 100.3c5 7.2 10.2 14.3 15.4 21.4l0 0c19.8 27.1 39.7 54.4 49.2 86.2H272zM192 512c44.2 0 80-35.8 80-80V416H112v16c0 44.2 35.8 80 80 80zM112 176c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-61.9 50.1-112 112-112c8.8 0 16 7.2 16 16s-7.2 16-16 16c-44.2 0-80 35.8-80 80z"></path></svg>`
     Output += "</div>"
 
-    Output += "<div id='LightModeToggleDark' class='HeaderObject HoverAccentColor CursorPointer' onclick='SetLightMode(!UseLightMode)' data-tooltip='" + Localize(Locale, "tooltip_lightmode") + "'>"
+    Output += "<div id='LightModeToggleDark' class='HeaderObject HoverAccentColor CursorPointer' data-tooltip='" + Localize(Locale, "tooltip_lightmode") + "'>"
     Output += `<svg class="HeaderSpecial" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path></svg>`
+    Output += "</div>"
     Output += "</div>"
 
     if (!isHomepage) {

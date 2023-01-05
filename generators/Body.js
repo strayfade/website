6501c83207d69796dc5305e9f54e6884fa52dc46
Noah Taylor <epicplayvig@gmail.com>
2023-01-05 09:12:02 -0500

    Update Body.js

--- generators/Body.js
+++ generators/Body.js
@@ -88,7 +88,7 @@ function GenerateBody(Article, Locale, AvailablePages, AvailablePageSelector, Cu
 
             Output += "<div class='HeroBannerLeft'>"
             Output += "<h1>About Me</h1>"
-            Output += "<p><span>Hello! I am Noah, a </span><span>" + CalculateAge() + "-year-old </span><span>software developer (primarily web development) and musician from the United States.</span></p>"
+            Output += "<p><span>Hello! I am Noah, a </span><span data-tooltip='Automatically Updates'>" + CalculateAge() + "-year-old </span><span>software developer (primarily web development) and musician from the United States.</span></p>"
             Output += "<p>You can send me an email at <a href='mailto:me@strayfade.com'>me@strayfade.com</a> and I might read it, or you can shoot me a message on <a href='https://twitter.com/Strayfade'>Twitter</a> or <a href='https://instagram.com/Strayfade'>Instagram</a>.</p>"
             Output += "<p>My current Discord tag is <a href='https://discord.com/users/455790298082181120'>Strayfade#8472</a>, although I do not usually respond to direct messages on Discord.</p>"
             Output += "</div>"

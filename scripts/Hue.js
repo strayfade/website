fa6337af0d27a1224093db395ad0b7754e49a341
Noah <epicplayvig@gmail.com>
2023-03-30 15:06:45 -0400

    Better Tags (again)

--- scripts/Hue.js
+++ scripts/Hue.js
@@ -5,26 +5,7 @@ function SetHue() {
     document.querySelector(':root').style.setProperty('--accent-color-hue', progress);
 }
 
-function SetInvert() {
-    const date = new Date();
-    if (date.getHours() > 6) {
-        document.getElementById("Slide1").style.backgroundColor = "black"
-        document.getElementById("SlideContent1").style.filter += " invert(1)"
-        let Affected = document.getElementsByClassName("Coloring1") 
-        for (let x = 0; x < Affected.length; x++) {
-            Affected[x].style.color = "white"
-            Affected[x].style.fill = "white"
-        }
-        let Unaffected = document.getElementsByClassName("UnaffectedDarkMode") 
-        for (let x = 0; x < Unaffected.length; x++) {
-            Unaffected[x].style.filter += " invert(1)"
-        }
-        document.body.style.backgroundColor = "black"
-    }
-}
-
 SetHue()
-SetInvert()
 setInterval(function() {
     SetHue()
 }, 10000)

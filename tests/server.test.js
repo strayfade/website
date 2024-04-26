b7d1aa13e6dbf0e7c8b9b89b3f6e0743d947fb1a
strayfade <64668046+strayfade@users.noreply.github.com>
2024-04-26 11:24:00 -0400

    Repair tests (test)

--- tests/server.test.js
+++ tests/server.test.js
@@ -16,32 +16,28 @@ describe('Server', () => {
             .get('/build/production.css')
             .expect('Content-Type', /^text\/css/)
             .expect(200)
-    })
+    });
     // Check homepage (/)
     it('serves the homepage properly', async () => {
         await request(Server)
             .get('/')
             .expect('Content-Type', /^text\/html/)
             .expect(200)
-    })
+    });
     // Check hidden pages
     it('serves secrets properly', async () => {
         await request(Server)
             .get('/R')
             .expect('Content-Type', /^text\/html/)
             .expect(200)
-    })
+    });
     // Check posts
-    it(`serves all posts properly`, async () => {
-        fs.readdir(path.join(__dirname, `../posts`), async (Error, Files) => {
-            for (const File of Files) {
-                await request(Server)
-                    .get(`/${File.split('.')[0]}`)
-                    .expect('Content-Type', /^text\/html/)
-                    .expect(200)
-            }
-        })
-    })
+    it(`serves the post "500" properly`, async () => {
+        await request(Server)
+            .get(`/500`)
+            .expect('Content-Type', /^text\/html/)
+            .expect(200)
+    });
     it('serves 404 page when route does not exist', async () => {
         await request(Server)
             .get('/nonExistentRoute')

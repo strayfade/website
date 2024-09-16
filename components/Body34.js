288e4f4ee92fd4ab22b1dbb29afd166e58b7ccfa
strayfade <64668046+strayfade@users.noreply.github.com>
2024-09-16 14:57:25 -0400

    Delete Body34.js

--- components/Body34.js
+++
@@ -1,9 +0,0 @@
-const Body34 = async (Request, Inner) => {
-    return `
-    <body style="--foreground: var(--dark); --background: #aae5a4; --font-family: verdana, sans-serif;">
-        ${Inner}
-        <script src="/prod.js"></script>
-    </body>
-    `
-}
-module.exports = { Body34 }

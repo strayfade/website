440672d5a969d02a355e62715799b1b4d5c389d6
strayfade <64668046+strayfade@users.noreply.github.com>
2024-09-21 20:37:52 -0400

    noscript warning

--- components/Body.js
+++ components/Body.js
@@ -1,6 +1,11 @@
 const Body = async (Request, Inner) => {
     return `
     <body${(Request.cookies && Request.cookies.lightMode && (Request.cookies.lightMode == "true")) ? ` style="--foreground: var(--light);--background: var(--dark);"` : ``}>
+        <noscript>    
+            <div class="banner">
+                You need Javascript enabled to use this webpage!
+            </div>
+        </noscript>
         ${Inner}
         <script src="/prod.js"></script>
     </body>
